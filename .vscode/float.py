edeca5cc830178f9b0ba8a3ca1066c82f9b6fe30
KarimOmar8 <113107665+KarimOmar8@users.noreply.github.com>
2023-04-19 11:49:42 +0200

    float

---
+++ .vscode/float.py
@@ -0,0 +1,21 @@
+# declaring float variables
+x = 3.1415
+y = 2.71828
+
+# performing calculations with floats
+result = x + y
+print(result) # output: 5.85978
+
+result = x * y
+print(result) # output: 8.53972922
+
+result = y / x
+print(result) # output: 0.8652600833568355
+
+# comparing floats
+if x < y:
+    print("x is less than y")
+elif x > y:
+    print("x is greater than y")
+else:
+    print("x and y are equal")
