94ba2ae52720c05468aa1aaa3028dcb6edc2164b
KarimOmar8 <113107665+KarimOmar8@users.noreply.github.com>
2023-04-19 10:22:16 +0200

    String

---
+++ .vscode/String.py
@@ -0,0 +1,15 @@
+# Strings definiëren en afdrukken
+my_string = "Dit is een string in Python."
+print(my_string)
+
+# Strings samenvoegen (concatenation)
+string_1 = "Hallo"
+string_2 = "wereld"
+hello_world = string_1 + " " + string_2
+print(hello_world)
+
+# Strings indexeren
+my_string = "eerste 6 letters!"
+print(my_string[0])    
+print(my_string[-1])   
+print(my_string[0:6])  
