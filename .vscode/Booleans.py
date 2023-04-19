f88fafbc3922615989381079df1239d477c57621
KarimOmar8 <113107665+KarimOmar8@users.noreply.github.com>
2023-04-19 11:45:57 +0200

    Booleans

---
+++ .vscode/Booleans.py
@@ -0,0 +1,12 @@
+x = 5
+y = 10
+z = x < y  # z wordt True omdat x kleiner is dan y
+print(z)  # output: True
+
+a = True
+b = False
+c = a and b  # c wordt False omdat a en b niet allebei True zijn
+print(c)  # output: False
+
+d = not a  # d wordt False omdat not a het tegenovergestelde van True is
+print(d)  # output: False
